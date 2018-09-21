6c48d4143c4c0994623bd2ea6ad7a19ddc2ade80
arakawamoriyuki <arakawamoriyuki@gmail.com>
2018-09-22 00:49:23 +0900

    zip解凍。pathやファイル名意識しなくていい

--- src/inception/inception.go
+++ src/inception/inception.go
@@ -6,21 +6,63 @@ import (
 	"net/http"
 )
 
-func DownloadModelZip(path string) error {
-	const url string = "https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip"
+type ModelFile struct {
+	DownloadUrl string
+	ZipPath string
+	UnzipPath string
+	LabelPath string
+	Path string
+}
 
-	response, err := http.Get(url)
+var modelFile ModelFile = ModelFile {
+	DownloadUrl: "https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip",
+	ZipPath: "inception5h.zip",
+	UnzipPath: "./",
+	LabelPath: "imagenet_comp_graph_label_strings.txt",
+	Path: "tensorflow_inception_graph.pb",
+}
+
+func ModelZipExists() bool {
+	return FileExists(modelFile.UnzipPath + modelFile.ZipPath)
+}
+
+func ModelExists() bool {
+	return FileExists(modelFile.UnzipPath + modelFile.LabelPath) &&
+		FileExists(modelFile.Path)
+}
+
+func DownloadModelZip() error {
+	response, err := http.Get(modelFile.DownloadUrl)
 	if err != nil {
 		return err
 	}
 	defer response.Body.Close()
 
-	file, err := os.Create(path)
+	file, err := os.Create(modelFile.UnzipPath)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
 	io.Copy(file, response.Body)
+	return nil
+}
+
+func SetUp() error {
+	modelExists := ModelExists()
+	if modelExists == false {
+		modelZipExists := ModelZipExists()
+		if modelZipExists == false {
+			err := DownloadModelZip()
+			if err != nil {
+				return err
+			}
+		}
+		err := Unzip(modelFile.ZipPath, modelFile.UnzipPath)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
--- src/main/main.go
+++ src/main/main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	util "github.com/arakawamoriyuki/go-tensorflow/src/util"
 	inception "github.com/arakawamoriyuki/go-tensorflow/src/inception"
 )
 
 func main() {
-	const path string = "inception5h.zip"
-
-	modelExists := util.FileExists(path)
-	if modelExists == false {
-		err := inception.DownloadModelZip(path)
-		if err != nil {
-			panic(err)
-		}
+	err := inception.SetUp()
+	if err != nil {
+		panic(err)
 	}
 }
 
